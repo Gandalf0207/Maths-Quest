6eb2c644cb830a6eb8ed8d6b5965c36acf45f98a
Gandalf0207 <quentin.pladeau@gmail.com>
2024-05-09 15:47:37 +0200

    patch v2 custom laby placement pnj

--- fichiers test/fuul_run_laby.py
+++ fichiers test/fuul_run_laby.py
@@ -1,5 +1,96 @@
 import random
+import time
+def mapmaker(longueur, largeur):
 
+    if longueur %2==0:
+        longueur +=1
+
+    if largeur %2==0:
+        largeur+=1
+
+
+    L = []
+    # generation de la map
+    for haut in range(largeur):
+        l = []
+
+        if haut%2 ==0:
+            for long in range(longueur):
+                l.append("■")
+            L.append(l)
+        else:
+            for long in range(longueur):
+                if long%2==0:
+                    l.append("■")
+                else:
+                    l.append(" ")
+            L.append(l)
+
+
+    # on setup les caractères aléatoirement (ASCII) dans les cases vide pour pouvoir générer le labyrinthe
+    comp = 0
+    for x in range(len(L)):
+        for y in range(len(L[x])):
+            if L[x][y] ==" ":
+                L[x][y] = chr(ord("0") + comp)
+                comp+=1
+
+
+    go = True
+    # on choisit aléatoirement un mur dans la map (ors côté) 
+    while go==True:
+
+        comparateur_1 = -1
+        comparateur_2 = -1
+        for x in range(len(L)):
+            for y in range(len(L[x])):
+                if L[x][y] !="■":
+                    if comparateur_1 == -1:
+                        comparateur_1 = L[x][y] 
+                    elif L[x][y] != comparateur_1:
+                        comparateur_2 = L[x][y]
+        if comparateur_2 == -1:
+            go = False
+
+
+
+    # Choix des coordonné aléatoirement dans la map
+        x = random.randint(1,largeur-2)
+        y = random.randint(1,longueur-2)
+
+        if L[x-1][y] != "■" and L[x+1][y] != "■":
+            cell_1 = L[x-1][y]
+            cell_2 = L[x+1][y]
+        elif L[x][y-1] != "■" and L[x][y+1] != "■":
+            cell_1 = L[x][y-1]
+            cell_2 = L[x][y+1]
+        else:
+            continue  # Skip this iteration as we cannot compare a cell with a wall       # ce bout de code a été réalisé avec copilot et légèrement modifier par moi 
+
+
+        # on les compare et on agit en fonction
+
+        if cell_1 != cell_2:
+
+            L[x][y] = cell_1
+            i = 1
+            while i < (largeur-1):
+                j = 1
+                while j < (longueur-1):
+                    if (L[i][j]==cell_2):
+                        L[i][j] = cell_1
+                    j+=1
+                i+=1
+            cell_1 = cell_1
+
+
+    #on remplace les cases de passage par des espace vides pour ue meilleur vision
+    for x in range(len(L)):
+        for y in range(len(L[x])):
+            if L[x][y] != "■":
+                L[x][y] = " "
+
+    return L
 
 def Custom_Map(Map, longueur, largeur, Niveau):
 
@@ -79,3 +170,22 @@ def Custom_Map(Map, longueur, largeur, Niveau):
 
 
     return Map
+
+
+Niveau = 0
+#On crée la map 
+longueur = 38
+largeur = 21
+aaa = 0
+while aaa != 10000:
+
+    L = mapmaker(longueur,largeur) 
+
+    #On custom la map avec les pnj, portes et joueur....
+    L = Custom_Map(L, longueur, largeur, Niveau)
+    #On affiche dans le terminal pour check
+    for i in range(len(L)):
+        print(*L[i], sep=" ")
+    
+    aaa+=1
+    
