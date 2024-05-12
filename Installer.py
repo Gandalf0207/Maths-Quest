90e0b9f76bf6d7071e23a4d4c5842694d77eb9bd
Gandalf0207 <quentin.pladeau@gmail.com>
2024-05-12 21:16:48 +0200

    add insqtaller .py

--- Installer.py
+++ Installer.py
@@ -2,18 +2,27 @@
 import subprocess
 
 
-def installer_pip_et_bibliotheques():
-    # Installer pip
-    subprocess.run(["python", "-m", "ensurepip"])
+def installateur():
+    # on recupère le jeu
+    subprocess.run("git clone https://github.com/Gandalf0207/Projet-Ouvert-Jeu-.git", shell=True, text=True)
 
+    #Installation de pytohn 3.12.3 (dernière version (modif name ?))
+    python_installer = "python-3.12.3-amd64.exe"
+    subprocess.call(python_installer, shell=True)
+    # Installer pip
+    subprocess.run(["python", "-m", "ensurepip"], shell=True)
     # Installer les bibliothèques requises
-    subprocess.run(["pip", "install", "matplotlib"])
+    subprocess.run(["pip", "install", "matplotlib"], shell=True)
+
+    # Installation miktek : compilateur latex
+    subprocess.run("basic-miktex-24.1-x64.exe", shell=True)
     # Ajoutez d'autres bibliothèques au besoin
-    subprocess.run(["mpm", "--install", "type1cm"])
-    subprocess.run(["mpm", "--install", "cm-super"])
-    subprocess.run(["mpm", "--install", "geometry"])
-    subprocess.run(["mpm", "--install", "underscore"])
-    subprocess.run(["mpm", "--install", "zhmetrics"])
+    subprocess.run(["mpm", "--install", "type1cm"], shell=True)
+    subprocess.run(["mpm", "--install", "cm-super"], shell=True)
+    subprocess.run(["mpm", "--install", "geometry"], shell=True)
+    subprocess.run(["mpm", "--install", "underscore"], shell=True)
+    subprocess.run(["mpm", "--install", "zhmetrics"], shell=True)
+
 
 # type1cm.sty
 # type1cm
@@ -36,5 +45,5 @@ def installer_pip_et_bibliotheques():
 
 
 if __name__ == "__main__":
-    installer_pip_et_bibliotheques()
+    installateur()
     print("Pip et les bibliothèques requises ont été installés avec succès !")
